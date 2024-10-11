56b51f525c3238a6d6484dd06eb4bec7233fa4ad
tusharm03 <muppidi.tushar@gmail.com>
2024-10-11 11:31:53 -0500

    finished assignment

--- src/main/java/com/csc/Zipper.java
+++ src/main/java/com/csc/Zipper.java
@@ -1,5 +1,25 @@
 package com.csc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zipper {
 
+    public static <T> List<T> zip(List<T> list1, List<T> list2) {
+        List<T> mergedList = new ArrayList<>();
+        int minLength = Math.min(list1.size(), list2.size());
+
+        for (int i = 0; i < minLength; i++) {
+            mergedList.add(list1.get(i));
+            mergedList.add(list2.get(i));
+        }
+
+        if (list1.size() > minLength) {
+            mergedList.addAll(list1.subList(minLength, list1.size()));
+        } else if (list2.size() > minLength) {
+            mergedList.addAll(list2.subList(minLength, list2.size()));
+        }
+
+        return mergedList;
+    }
 }
--- src/test/java/com/csc/ZipperTest.java
+++ src/test/java/com/csc/ZipperTest.java
@@ -1,21 +1,65 @@
 package com.csc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ZipperTest {
 
-  Zipper zipper;
+    Zipper zipper;
 
-  @BeforeEach
-  void setUp() {
-    zipper = new Zipper();
-  }
+    @BeforeEach
+    void setUp() {
+        zipper = new Zipper();
+    }
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
+    @Test
+    void testZipWithEqualLengthLists() {
+        List<Integer> nums1 = List.of(1, 3, 5, 7);
+        List<Integer> nums2 = List.of(2, 4, 6, 8);
+        List<Integer> mergedNumbers = zipper.zip(nums1, nums2);
+        assertEquals(List.of(1, 2, 3, 4, 5, 6, 7, 8), mergedNumbers);
+    }
+
+    @Test
+    void testZipWithFirstListLonger() {
+        List<String> colors1 = List.of("Red", "Green", "Blue");
+        List<String> colors2 = List.of("White", "Black");
+        List<String> mergedColors = zipper.zip(colors1, colors2);
+        assertEquals(List.of("Red", "White", "Green", "Black", "Blue"), mergedColors);
+    }
+
+    @Test
+    void testZipWithSecondListLonger() {
+        List<Integer> nums1 = List.of(1, 3);
+        List<Integer> nums2 = List.of(2, 4, 6, 8);
+        List<Integer> mergedNumbers = zipper.zip(nums1, nums2);
+        assertEquals(List.of(1, 2, 3, 4, 6, 8), mergedNumbers);
+    }
+
+    @Test
+    void testZipWithEmptyFirstList() {
+        List<String> colors1 = List.of();
+        List<String> colors2 = List.of("White", "Black");
+        List<String> mergedColors = zipper.zip(colors1, colors2);
+        assertEquals(List.of("White", "Black"), mergedColors);
+    }
+
+    @Test
+    void testZipWithEmptySecondList() {
+        List<String> colors1 = List.of("Red", "Green");
+        List<String> colors2 = List.of();
+        List<String> mergedColors = zipper.zip(colors1, colors2);
+        assertEquals(List.of("Red", "Green"), mergedColors);
+    }
+
+    @Test
+    void testZipWithBothEmptyLists() {
+        List<String> colors1 = List.of();
+        List<String> colors2 = List.of();
+        List<String> mergedColors = zipper.zip(colors1, colors2);
+        assertEquals(List.of(), mergedColors);
+    }
 }
